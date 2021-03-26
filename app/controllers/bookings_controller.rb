25cbc1fbc7b4a0ef41e9e8ce0ff0c0fb068486bd
Spike Vinz Cruz <spikevinz@gmail.com>
2021-03-26 08:03:38 +0800

    finished main tasks

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,31 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @booking.passengers.build
+    @flight_id = params[:flight_id]
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.passengers.build(params[:booking][:passengers])
+    if @booking.save
+      redirect_to @booking
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def ticket
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
